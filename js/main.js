35cebe39f94f49befa61527128e24ff82d87a064
kameoka <e2058232@soka-u.jp>
2024-06-09 08:03:26 +0900

    feat: Update main.js to dynamically populate station names in HTML table
    This commit modifies the main.js file to dynamically populate the HTML table with station names. The  function is added to handle this functionality. It takes an array of station data and updates the table rows with the corresponding station names. This enhancement improves the user experience by displaying real-time station information on the web application.

--- js/main.js
+++ js/main.js
@@ -9,17 +9,33 @@ document.addEventListener("DOMContentLoaded", () => {
 // ロケーション情報を取得する
 const getLocation = async () => {
   const location_json_data = await getapi(location_url);
+  // 編集できるlocation_json
+  let location_json_data_edit = location_json_data;
   let station_list = [];
+  // console.log(location_json_data);
   location_json_data.forEach((dict_data) => {
     station_list.push(dict_data["odpt:fromStation"]);
-    // station_list.push(dict_data["odpt:toStation"]);
+    station_list.push(dict_data["odpt:toStation"]);
   });
+  // station_listの重複を消す
+  station_list = Array.from(new Set(station_list));
+  console.log(station_list);
   const station_str = station_list.join(",");
   const station_api_url = `${station_url}&owl:sameAs=${station_str}`;
-  console.log(station_api_url);
   const station_json_data = await getapi(station_api_url);
-  station_json_data.forEach((dict_data) => {
-    console.log(dict_data["dc:title"]);
+  // console.log(station_json_data);
+  location_json_data_edit.forEach((dict_data) => {
+    from_station_data = station_json_data.find((station) => station["owl:sameAs"] === dict_data["odpt:fromStation"]);
+    to_station_data = station_json_data.find((station) => station["owl:sameAs"] === dict_data["odpt:toStation"]);
+    dict_data["odpt:fromStation"] = from_station_data == null ? null : from_station_data["dc:title"];
+    dict_data["odpt:toStation"] = to_station_data == null ? null : to_station_data["dc:title"];
+    // dict_data["odpt:toStation"] = station_json_data.find((station) => station["owl:sameAs"] === dict_data["odpt:toStation"])["dc:title"];
   });
-  modifyHTML(station_json_data);
+  modifyHTML(location_json_data_edit);
+  // console.log(location_json_data_edit);
+
+  // station_json_data.forEach((dict_data) => {
+  //   console.log(dict_data["dc:title"]);
+  // });
+  // modifyHTML(station_json_data);
 };
--- js/pushhtml.js
+++ js/pushhtml.js
@@ -1,13 +1,15 @@
 // Modify the HTML data
 function modifyHTML(data) {
   const element = document.getElementById("station");
-  //   const data_count = data.length;
   //   データ数分だけの表を作成
   data.forEach((dict_data) => {
     const tr = document.createElement("tr");
-    const td = document.createElement("td");
-    tr.appendChild(td);
-    td.textContent = dict_data["dc:title"];
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    td1.textContent = dict_data["odpt:fromStation"];
+    td2.textContent = dict_data["odpt:toStation"];
     element.appendChild(tr);
   });
 }
